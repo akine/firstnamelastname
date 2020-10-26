0ba0ee8ee7712bb45713b8b2d145c324f13aeac1
akine <weezer.and.you@gmail.com>
2020-10-26 22:14:53 +0900

    first init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+
+function UsernameForm({ onSubmitUsername }) {
+  return (
+    <form>
+      <div>
+        <label>Username:</label>
+        <input type="text" />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
 
 function App() {
+  const onSubmitUsername = (username) => alert(`You entered: ${username}`);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <UsernameForm onSubmitUsername={onSubmitUsername} />
       </header>
     </div>
   );
