050368feca606dcb05ffa7118df98d7f096df5a6
akine <weezer.and.you@gmail.com>
2020-10-27 08:54:03 +0900

    add: input form

--- src/App.js
+++ src/App.js
@@ -2,19 +2,31 @@ import "./App.css";
 import React from "react";
 
 function UsernameForm({ onSubmitUsername }) {
+  function hundleSubmit(event) {
+    event.preventDefault();
+    // console.dir(event.target)
+    const lastname = event.target.elements.lastnameInput.value;
+    const firstname = event.target.elements.firstnameInput.value;
+    onSubmitUsername(lastname,firstname);
+  }
   return (
-    <form>
+    <form onSubmit={hundleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="lastnameInput">名字: </label>
+        <input id="lastnameInput" type="text" />
       </div>
-      <button type="submit">Submit</button>
+      <div>
+        <label htmlFor="firstnameInput">名前: </label>
+        <input id="firstnameInput" type="text" />
+      </div>
+      <button type="submit">ファーストネームラストネーム知りたい</button>
     </form>
   );
 }
 
 function App() {
-  const onSubmitUsername = (username) => alert(`You entered: ${username}`);
+  const onSubmitUsername = (firstname, lastname) =>
+    alert(`お前のラストネームは: ${lastname}\nんでファーストネームは: ${firstname}`);
   return (
     <div className="App">
       <header className="App-header">
